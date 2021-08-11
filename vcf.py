3ea082ed775af9f1d3e4236732dcd94c111d804b
Lilli Schuckert <83643736+slilli@users.noreply.github.com>
2021-08-11 15:29:14 +0200

    Add files via upload

--- vcf.py
+++ vcf.py
@@ -1,19 +1,22 @@
 #vcf file handling
-
 #import modules ------------------
+
 
 import allel
 import sys
 import numpy as np
+import pandas as pd
+
 
 #---------------------------------
+
 
 def setup():
 
 
 	if not len(sys.argv) == 2:
 		print("Error: 2 arguments needed")
-		print("[(...).py][input_vcf]")
+		print("Your input should look like: [(...).py][input_vcf]")
 		sys.exit()
 	
 
@@ -21,36 +24,54 @@ def setup():
 		print("opened vcf")
 
 
-def extract_snps(s):
+def extract_snps():
 
-	
+
+	callset = allel.read_vcf(sys.argv[1])
+	id_from_vcf = callset['variants/ID']
+
+
 	print("Loading all snps into an array...")
-	print(s)
-	print("vcf_ID_length:", len(s))
-	
+	print(id_from_vcf)
+	print("vcf_ID_length:", len(id_from_vcf))
 
-def select_id():
+
+def filtered_id():
 
 	
 	callset = allel.read_vcf(sys.argv[1])
-	id_from_vcf = np.array(callset['variants/ID'])
+	id_from_vcf = callset['variants/ID']
+	ref_from_vcf = callset['variants/REF']
+	alt_from_vcf = callset['variants/ALT']
+	
+
+	relevant_ID_for_ABO = []
 
 
-	vcf_ID_new = []
+	counter = 0
+	counter2 = 0
 
 
-	for k in id_from_vcf:
-		#if (k[0] == 'r') and (k[1] == 's'):
-			vcf_ID_new = np.append(id_from_vcf[k])
-			print("vcf_ID_rs:", len(vcf_ID_new)) 
+	for k in [0,len(id_from_vcf)-1]:
+		if(id_from_vcf[k] == "rs8176747") or (id_from_vcf[k] == "rs8176747") or (id_from_vcf[k] == "rs8176719"):
+			counter2 += 1
+			relevant_ID_for_ABO = np.append(relevant_ID_for_ABO, id_from_vcf[k])		
+
+
+	print("relevant_ID_length:", len(relevant_ID_for_ABO)) 
+	print(relevant_ID_for_ABO)
+	print("counter:", counter)
+	print("counter2:", counter2)
 
 
 #main-----------------------------
 
+
 if __name__ == "__main__":
 
-	callset = allel.read_vcf(sys.argv[1])
-	id_from_vcf = callset['variants/ID']
+
 	setup()
-	extract_snps(id_from_vcf)
-	select_id()
+	extract_snps()
+	filtered_id()
+
+
